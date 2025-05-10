c626dbc076dc9783652702c793464c61fb079e7f
mark-summerfield <mark@qtrac.eu>
2025-05-10 16:32:27 +0100

    m

---
+++ buildwin.sh
@@ -0,0 +1,8 @@
+env GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o cardindex.exe .
+go-winres simply --icon images/icon.ico
+CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc \
+    CXX=x86_64-w64-mingw32-g++ \
+    go build -buildvcs=false \
+      -ldflags="-s -w -H=windowsgui -extldflags=-static" \
+      -o cardindex.exe .
+strip cardindex.exe
